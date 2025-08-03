584a386b8b2cf965eacb01d043c0b8f38b0952d3
akarp0508 <akarp0508@gmail.com>
2025-08-03 23:20:55 +0200

    moved on to testing triangle borders drawing

--- src/main_loop.asm
+++ src/main_loop.asm
@@ -2,9 +2,17 @@ main_loop:
     ; render triangles
     ;call [r0,'draw_all_triangles']
     ; change PAGE_INDEX
-    mov r1 $005F002F
-    mov r3 $001F001F
-    call [r0,'follow_the_line']
+    mov r1 $00200020
+    mov r2 $00400050
+    mov r3 $00500030
+    mov r4 0
+    stw r1 [r4,PROJECTED_VERTICES]
+    add r4 4
+    stw r2 [r4,PROJECTED_VERTICES]
+    add r4 4
+    stw r3 [r4,PROJECTED_VERTICES]
+    mov r1 $00000102
+    call [r0,'generate_wireframe']
     jmp [r0,'main_loop']
 wait_for_gpu:
     ldbu r2 [r0,GPU_FLAGS]
